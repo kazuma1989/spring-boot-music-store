503f9c9e6e8b4dd1f96bb81e1128a040d42dc710
kazuma1989 <funifuni.1204@gmail.com>
2019-09-15 12:59:32 +0900

    feat: Add logo, menu, hero

---
+++ src/main/java/com/github/kazuma1989/sbms/cart/controller/CartSession.java
@@ -0,0 +1,12 @@
+
+package com.github.kazuma1989.sbms.cart.controller;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.SessionScope;
+
+@SessionScope
+@Component("cartSession")
+public class CartSession {
+
+    public int cartCount = 0;
+}
